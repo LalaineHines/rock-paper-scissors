0860a2ace19dbfe4f33ef27209e4ac9ca67c7f6c
LalaineHines <lalainehines@gmail.com>
2025-07-31 18:47:03 -0700

    added human and computer fucntions starting on playing a round function

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 console.log("Hello World!");
 
+// Computers function for rock, paper, scissors
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+    };
+
+// log to ensure function is working
+
+console.log(getComputerChoice());
+
+// Humans choice for rock, paper, scissors
+
+function getHumanChoice() {
+    let humanChoice = prompt("Choose your weapon: rock, paper, or scissors?").toLowerCase();
+
+// validate the input to make sure it's a valid choice
+    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
+        alert("invalid choice! please enter 'rock', 'paper', or 'scissors'.");
+        humanChoice = prompt("choose your weapon: rock, paper, or scissors?").toLowerCase;
+    };
+};
+
+// log to ensure that function is working
+
+console.log(getHumanChoice());
+
+// function for playing a round of rock, paper, scissors
+
